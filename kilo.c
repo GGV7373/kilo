a8cf01fadf90b91cb413390a6147c771ba2db41e
GGV7373 <ggvictor7373@outlook.com>
2026-09-19 22:23:17 +0200

    added more flags

--- kilo.c
+++ kilo.c
@@ -18,7 +18,10 @@ void enableRawMode () {
     atexit(disableRawMode);
     
     struct termios raw = orgi_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_lflag &= ~(OPOST);
+    raw.c_lflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON| IEXTEN |ISIG);
 
     tcsetattr(STDIN_FILENO, TCIFLUSH, &raw);
 }
@@ -29,9 +32,9 @@ int main() {
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c !='q') {
         if (iscntrl(c)) {
-                    printf("%d\n", c);
+                    printf("%d\r\n", c);
         } else {
-        printf("%d\n ('%c')\n", c, c);
+        printf("%d ('%c')\r\n", c, c);
         }
     }
 
