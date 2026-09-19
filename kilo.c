f899be31d91c8f4065eb240c056dbdd06b07efd2
GGV7373 <ggvictor7373@outlook.com>
2026-09-19 20:56:49 +0200

    added more to kilo.c

--- kilo.c
+++ kilo.c
@@ -1,8 +1,35 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+
+
+struct termios orgi_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCIFLUSH, &orgi_termios);
+}
+
+
+void enableRewMode () {
+    tcgetattr(STDIN_FILENO, &orgi_termios);
+    atexit(disableRawMode);
+    
+    struct termios raw = orgi_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+    
+    tcsetattr(STDIN_FILENO, TCIFLUSH, &raw);
+}
+
 int main() {
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    enableRewMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 
   return 0;
 }
